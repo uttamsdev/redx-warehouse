d92803bd05d2f27722059b77e9f8c22f0ca0253a
uttamsaha <uttamsaha982@gmail.com>
2022-05-05 08:02:47 +0600

    custom alert added

--- src/Pages/ManageInventory/SingleInventoryItem/SingleInventoryItem.js
+++ src/Pages/ManageInventory/SingleInventoryItem/SingleInventoryItem.js
@@ -2,25 +2,41 @@ import React from 'react';
 import './SingleInventoryItem.css';
 import {AiOutlineDelete} from 'react-icons/ai';
 import useProducts from '../../Hooks/useProducts';
+import swal from 'sweetalert';
+
 
 const SingleInventoryItem = ({product}) => {
     const {_id,name, img, description, quantity, supplierName, price} = product;
     const [products, setProducts] = useProducts(true);
 
     const handleDelete = (id) => {
-        const proceed = window.confirm("Are you sure you want to delete?");
-    if (proceed) {
-      const url = `http://localhost:5000/products/${id}`;
-      fetch(url, {
-        method: "DELETE",
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          const remaining = products.filter((product) => product._id !== id);
-          setProducts(remaining);
+      //sweet alert
+        swal({
+          title: "Are you sure?",
+          text: "Once deleted, you will not be able to recover",
+          icon: "warning",
+          buttons: true,
+          dangerMode: true,
+        })
+        .then((willDelete) => {
+          if (willDelete) {
+            swal("Your Item has been deleted successfully!", {
+              icon: "success",
+            });
+            const url = `http://localhost:5000/products/${id}`;
+            fetch(url, {
+              method: "DELETE",
+            })
+              .then((res) => res.json())
+              .then((data) => {
+                console.log(data);
+                const remaining = products.filter((product) => product._id !== id);
+                setProducts(remaining);
+              });
+          } else {
+            swal("Item not deleted. You cancelled.");
+          }
         });
-    }
     }
   return (
     <div className='outer-product'>
