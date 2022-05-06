02c72a2d6d8c1201b4d96caa62c9d7d047fece84
uttamsaha <uttamsaha982@gmail.com>
2022-05-06 12:31:00 +0600

    sotck add functionality done

--- src/Pages/Hooks/useProduct.js
+++ src/Pages/Hooks/useProduct.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from "react";
 
 const useProduct = (productId) => {
     const [product, setProduct] = useState([]);
-
     useEffect(()=>{
         fetch(`http://localhost:5000/products/${productId}`)
     .then(res => res.json())
     .then(data => setProduct(data))
-    },[productId])
-    return [product];
+    },[productId,product])
+   
+    return [product, setProduct];
 }
 export default useProduct;
--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import useProduct from '../Hooks/useProduct';
 import {HiArrowRight} from 'react-icons/hi';
 import './Inventory.css';
+import swal from 'sweetalert';
 
 const Inventory = () => {
     const {id} = useParams();
@@ -10,6 +11,37 @@ const Inventory = () => {
     const navigate = useNavigate();
     const manageInventories = () => {
         navigate('/manageInventories');
+    }
+
+    //handle stock add
+    const productQuantity = product.quantity;
+    console.log('product quantity:',productQuantity);
+
+    //handle update
+    const handleUpdate = (event) => {
+      event.preventDefault();
+      const restockQuantity = event.target.restock.value;
+      const updatedRestock = parseInt(productQuantity) + parseInt(restockQuantity);
+      const restock = updatedRestock.toString();
+      console.log('update restock:',restock);
+      const updateQuantity = {restock};
+
+      //update data
+      const url = `http://localhost:5000/products/${id}`;
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updateQuantity),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // setProduct(product);
+        swal("Quantity Updated!", "Quantity Successfully Updated!", "success");
+        event.target.reset();
+      });
     }
   return (
     <div className='product-information'>
@@ -22,7 +54,7 @@ const Inventory = () => {
         <p><b>Supplier name:</b> {product.supplierName}</p>
         <button className='delivered-btn'>Delivered</button>
         <button onClick={manageInventories} className='delivered-btn'>Manage Inventories <HiArrowRight/></button>
-        <form>
+        <form onSubmit={handleUpdate}>
             <label htmlFor="restock"><b>Restock the item:</b></label>
             <input className='item-quantity form-control' type="number" name="restock" id="" placeholder='Enter number of items'/>
             <input className='btn btn-dark mt-3 mb-5' type="submit" value="Add Quantity" />
